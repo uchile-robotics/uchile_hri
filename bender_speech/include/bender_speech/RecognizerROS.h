e73c20c68b62fc8d0abf52731d0c079fa6c36bde
ristofer <cristopher.gomez@ug.uchile.cl>
2016-10-19 18:06:36 -0300

    new speech recognition

---
+++ bender_speech/include/bender_speech/RecognizerROS.h
@@ -0,0 +1,72 @@
+#ifndef RECOGNIZERROS_HPP_
+#define RECOGNIZERROS_HPP_
+
+
+#include <ros/ros.h>
+
+#include <boost/thread/thread.hpp>
+
+#include <actionlib/server/simple_action_server.h>
+#include <bender_speech/DoRecognitionAction.h>
+#include <bender_speech/SpeechRecognitionConfig.h>
+
+#include <dynamic_reconfigure/server.h>
+
+#include <sstream>
+
+#include "bender_speech/Recognizer.h"
+
+typedef boost::shared_ptr<Recognizer> RecognizerPtr;
+
+class RecognizerROS 
+{
+public:
+
+    RecognizerROS();
+    ~RecognizerROS();
+    void executeCB(const bender_speech::DoRecognitionGoalConstPtr &goal);
+    void dynamicCallback(bender_speech::SpeechRecognitionConfig &config,uint32_t level);
+    void resetRecognizer();
+    void updateDirectories(std::string dictionary);
+    void recognize();
+
+private:
+
+    ros::NodeHandle nh_;
+    ros::Rate loop_rate_;
+    AudioSource as_;
+
+    actionlib::SimpleActionServer<bender_speech::DoRecognitionAction> actionServer_;
+
+    bender_speech::DoRecognitionFeedback feedback_;
+    bender_speech::DoRecognitionResult result_;
+
+    dynamic_reconfigure::Server<bender_speech::SpeechRecognitionConfig> parameterServer_;
+    dynamic_reconfigure::Server<bender_speech::SpeechRecognitionConfig>::CallbackType reconfigureCallback_;
+
+
+    std::string pkg_dir_;
+
+    std::string hmmdir_;
+    std::string modeldir_;
+    std::string grammardir_;
+    std::string dictdir_;
+    std::string threshold_;
+    std::string mic_name_;
+
+    double vad_thres_;
+    int vad_pre_;
+    int vad_post_;
+    int vad_start_;
+    
+    std::string final_result_;
+    std::string partial_result_;
+    
+    RecognizerPtr recognizer_;
+    bool in_speech_;
+    bool is_on_;
+    
+
+};
+
+#endif /* RECOGNIZERROS_HPP_ */
---
+++ bender_speech/src/Recognizer.cpp
@@ -0,0 +1,169 @@
+#include "bender_speech/Recognizer.h"
+#include <boost/lexical_cast.hpp>
+#include "bender_speech/Exceptions.h"
+
+Recognizer::Recognizer(AudioSource *as, 
+           std::string modeldir, 
+           std::string grammardir, 
+           std::string dictdir, 
+           double vad_thres,
+           int vad_pre,
+           int vad_post,
+           int vad_start):
+    as_(as),
+    modeldir_(modeldir),
+    grammardir_(grammardir),
+    dictdir_(dictdir)
+    {
+        threshold_ = boost::lexical_cast<std::string>(vad_thres);
+        prespeech_ = boost::lexical_cast<std::string>(vad_pre);
+        postspeech_ = boost::lexical_cast<std::string>(vad_post);
+        startspeech_ = boost::lexical_cast<std::string>(vad_start);
+       
+        //ROS_INFO_STREAM(threshold_.c_str());
+
+
+        config_ = cmd_ln_init(NULL, ps_args(), TRUE,
+        "-hmm", modeldir_.c_str(),
+        "-jsgf",grammardir_.c_str(),
+        "-dict",dictdir_.c_str() ,
+        "-vad_threshold",threshold_.c_str(),
+        "-vad_prespeech",prespeech_.c_str(),
+        "-vad_postspeech",postspeech_.c_str(),
+        "-vad_startspeech",startspeech_.c_str(),    
+        "-remove_noise","yes",
+        NULL);
+        
+        if (config_ == NULL)
+        {
+            throw init_config_error();
+            
+        }
+
+        ps_ = ps_init(config_);
+        if (ps_ == NULL) 
+        {
+            throw init_ps_decoder_error();
+        }
+
+        init_state_ = true;
+
+    }
+
+
+
+Recognizer::~Recognizer()
+{
+    cmd_ln_free_r(config_);
+    ps_free(ps_);
+    init_state_ = false;
+}
+
+
+
+void Recognizer::startUtt()
+{
+    if (ps_start_utt(ps_) < 0)
+    {
+        throw start_utt_error();
+    }
+}
+
+void Recognizer::proccesRaw()
+{
+    ps_process_raw(ps_, as_->buf(), as_->k(), FALSE, FALSE);
+}
+
+void Recognizer::endUtt()
+{
+    ps_end_utt(ps_);
+}
+
+
+std::string Recognizer::getHyp()
+{
+     char const *hyp;
+     hyp = ps_get_hyp(ps_, NULL);
+     if(hyp==NULL){
+        return std::string("");
+     }
+     return std::string(hyp);
+}
+
+bool Recognizer::inSpeech()
+{
+    if (ps_get_in_speech(ps_))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void Recognizer::initDevice(std::string device)
+{
+    as_->openDevice(device);
+    as_->startRec();
+}
+
+void Recognizer::readAudio()
+{
+    as_->read();
+}
+
+void Recognizer::terminateDevice()
+{
+    as_->closeDevice();
+}
+
+void Recognizer::setDict(const std::string& dictdir)
+{
+    dictdir_ = dictdir;
+    ps_load_dict(ps_,dictdir_.c_str(),NULL,NULL);
+}
+
+void Recognizer::setGrammar(const std::string& grammardir)
+{
+    grammardir_ = grammardir;
+    ps_set_jsgf_file(ps_,"grammar_search",grammardir_.c_str());    
+    ps_set_search(ps_,"grammar_search");
+}
+
+
+void Recognizer::setThreshold(const std::string& threshold)
+{
+    threshold_ = threshold;
+}
+
+
+bool Recognizer::status()
+{
+    return init_state_;
+}
+
+void Recognizer::update()
+{
+    if(init_state_){
+    cmd_ln_free_r(config_);
+    ps_free(ps_);
+    init_state_ = false;
+    }
+    
+}
+
+
+
+std::string Recognizer::getSearch()
+{
+    
+
+     char const *search_name;
+     search_name = ps_get_search(ps_);
+     if(search_name==NULL){
+        return std::string("");
+     }
+     return std::string(search_name);
+
+}

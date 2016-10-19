b35258a7f466c2247ae8aa853400ff2db1b2a9eb
ristofer <cristopher.gomez@ug.uchile.cl>
2016-10-19 17:37:35 -0300

    moving old bender_speech to bender_code_graveyard

--- bender_speech/bash/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-## Configurar el entorno con las variables necesarias para el package.
-# utilizados para ??
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig
-
-# directorio con instalación de LOGIOS (compilación de diccionarios)
-export BENDER_SPEECH_LOGIOS_DIR="$BENDER_WS"/install/soft/hri/speech/cmusphinx-code/logios

4250010317f19bc4be7d15a4f7247c4e4778627d
BenderUchile <benderuchile@gmail.com>
2016-11-05 20:01:23 -0300

    fixing bender_nlp problems

---
+++ bender_nlp/src/bender_nlp/tester.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+from bender_nlp import GenerateOrder
+
+
+while True:
+	sens = input('Write your sentence')
+	# Analize sentence and obtain orders
+	if sens=='q':
+		break
+	s = GenerateOrder(sens)
+
+	# Uncomment for view results
+	print sens
+	print '==========================='
+	print 'Verbs  : {}'.format(s.verbs)
+	print 'People : {}'.format(s.people)
+	print 'Objects: {}'.format(s.objects)
+	print 'Places : {}'.format(s.places)
+	print 'Info   : {}'.format(s.information)

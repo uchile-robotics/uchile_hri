3bcf3d5bde3125e07a7f2b48a7c7aec392a08008
matias pavez <matias.pavez.b@gmail.com>
2016-11-23 22:43:28 -0300

    instalador de bender speech

---
+++ bender_speech/README.md
@@ -0,0 +1,17 @@
+# bender_speech
+
+## Overview
+
+TODO
+
+
+## Installation
+
+```bash
+$ cdb bender_speech
+$ bash install/install.bash
+```
+
+This will download and setup a fork of the [sphinxbase](https://github.com/cmusphinx/sphinxbase) and [pocketsphinx](https://github.com/cmusphinx/pocketsphinx) repositories, maintained at the [github page](https://github.com/uchile-robotics-die) of the team.
+
+See [this link](http://cmusphinx.sourceforge.net/wiki/tutorialpocketsphinx) for an alternative installation guide.
---
+++ bender_speech/install/install.bash
@@ -0,0 +1,147 @@
+#!/bin/bash
+#
+# run me like this:
+# > sudo apt-get update
+# > cdb bender_tts
+# > bash install/install.sh
+
+
+## ----------------------------------------------------------------------------
+## Useful Variables
+## ----------------------------------------------------------------------------
+
+# package name
+pkg_name="bender_speech"
+
+# package path
+pkg_path=$(rospack find "$pkg_name")
+
+
+## ----------------------------------------------------------------------------
+## User Configuration 
+## ----------------------------------------------------------------------------
+
+# where download and mantain the files
+install_space="$BENDER_WS"/install/soft/hri/speech
+
+# mark as installed 
+install_token="$install_space"/INSTALLED
+
+
+## ----------------------------------------------------------------------------
+## Dependencies
+## ----------------------------------------------------------------------------
+
+# speech recognizer (pocketsphinx)
+#sudo apt-get install gcc automake autoconf libtool bison swig unzip
+#sudo apt-get install bison
+
+
+## ----------------------------------------------------------------------------
+## Install
+## ----------------------------------------------------------------------------
+mkdir -p "$install_space" && cd "$install_space"
+
+
+## DOWNLOAD sphinxbase or update it if neccesary
+echo ""
+cd "$install_space"
+if [ ! -d "sphinxbase" ]; then
+	echo "sphinxbase repository not found.... cloning..."
+	git clone https://github.com/uchile-robotics-die/sphinxbase.git
+else
+	echo "sphinxbase repository already exists on $install_space"
+	echo "updating ..."
+	
+	cd sphinxbase
+	git remote update
+	LOCAL=$(git rev-parse @)
+	REMOTE=$(git rev-parse "@{u}")
+	BASE=$(git merge-base @ "@{u}")
+
+	if [ "$LOCAL" = "$REMOTE" ]; then
+	    echo "sphinxbase is up-to-date"
+
+	elif [ "$LOCAL" = "$BASE" ]; then
+		# update and mark as not ready
+		echo "sphinxbase needs to be updated ... "
+	    git pull origin master
+	    rm -f "$install_space/$install_token"
+
+	elif [ "$REMOTE" = "$BASE" ]; then
+	    echo "Need to push"
+	    # do nothing
+	else
+	    echo "Diverged"
+	    # do nothing
+	fi
+fi
+
+## DOWNLOAD pocketsphinx or update it if neccesary
+echo ""
+cd "$install_space"
+if [ ! -d "pocketsphinx" ]; then
+	echo "pocketsphinx repository not found.... cloning..."
+	git clone https://github.com/uchile-robotics-die/pocketsphinx.git
+else
+	echo "pocketsphinx repository already exists on $install_space"
+	echo "updating ..."
+
+	cd "$install_space"
+	cd pocketsphinx
+	git remote update
+	LOCAL=$(git rev-parse @)
+	REMOTE=$(git rev-parse "@{u}")
+	BASE=$(git merge-base @ "@{u}")
+
+	if [ "$LOCAL" = "$REMOTE" ]; then
+	    echo "pocketsphinx is up-to-date"
+
+	elif [ "$LOCAL" = "$BASE" ]; then
+		# update and mark as not ready
+		echo "pocketsphinx needs to be updated ... "
+	    git pull origin master
+	    rm -f "$install_space/$install_token"
+
+	elif [ "$REMOTE" = "$BASE" ]; then
+	    echo "Need to push"
+	    # do nothing
+	else
+	    echo "Diverged"
+	    # do nothing
+	fi
+fi
+
+## exit if already installed
+cd "$install_space"
+if [ -e "$install_token" ]; then
+	echo ""
+	echo " - ---------------------------------------------------------------------"
+	echo " - sphinxbase and pocketsphinx are already installed and up to date. Bye"
+	echo " - ---------------------------------------------------------------------"
+	exit 0
+fi
+
+## Installing sphinxbase
+## -------------------------------------
+cd "$install_space"/sphinxbase
+./autogen.sh
+./configure
+make
+sudo make install
+
+## Installing pocketsphinx
+## -------------------------------------
+cd "$install_space"/pocketsphinx
+./autogen.sh
+./configure
+make
+sudo make install
+
+## mark as installed
+cd "$install_space"
+touch "$install_token"
+
+echo ""
+echo "done"
+echo ""

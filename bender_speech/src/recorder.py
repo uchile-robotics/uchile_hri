43d81ecdba7d27ba14de3f7339eeb0e592a7b46a
ristofer <cristopher.gomez@ug.uchile.cl>
2017-01-05 15:11:21 -0300

    minor fixes in the recording script

--- bender_speech/src/recorder.py
+++ bender_speech/src/recorder.py
@@ -6,23 +6,23 @@ from std_msgs.msg import Empty, String
 
 class Recorder:
     def __init__(self):
-        self.inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE)
-        self.inp.setchannels(1)
-        self.inp.setrate(16000)
-        self.inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
-        self.inp.setperiodsize(1024)
         self.record_sub = rospy.Subscriber("record", String, self.recordCallback)
         self.stop_sub= rospy.Subscriber("stop",Empty,self.stopCallback)
         self.stoped = False
     def recordCallback(self,msg):
         rospy.loginfo("Recording...")
+        inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE)
+        inp.setchannels(1)
+        inp.setrate(16000)
+        inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+        inp.setperiodsize(1024)
         self.stoped = False
         w = wave.open(msg.data, 'w')
         w.setnchannels(1)
         w.setsampwidth(2)
         w.setframerate(16000)
         while not self.stoped:
-            l, data = self.inp.read()
+            l, data = inp.read()
             w.writeframes(data)
         w.close()
         rospy.loginfo("Finished")

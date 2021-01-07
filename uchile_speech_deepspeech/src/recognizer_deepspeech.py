61167d1635de2ff60eae017012464e3ee8d4f140
Javier Urrutia <javier.urrutia.r@ug.uchile.cl>
2021-01-07 18:58:29 +0000

    Added preemt request case

--- uchile_speech_deepspeech/src/recognizer_deepspeech.py
+++ uchile_speech_deepspeech/src/recognizer_deepspeech.py
@@ -28,7 +28,6 @@ VAD_AGG = 3
 class SpeechRecognitionServer:
     def __init__(self):
         self.recognition_server = actionlib.SimpleActionServer('~recognizer_action', DoRecognitionAction, self.execute, False)
-        self.recognition_server.start()
 
         # Load deepspeech model
         rospack = rospkg.RosPack()
@@ -43,6 +42,7 @@ class SpeechRecognitionServer:
 
 
         self.recognition_response = DoRecognitionResult()
+        self.recognition_server.start()
 
 
     def terminate(self):
@@ -79,15 +79,20 @@ class SpeechRecognitionServer:
         rospy.loginfo('Listening ...')
         t_start = time.time()
         start_speech = False
+        success = True
         while sm_vad.current_state_id != vad_sm.ENDSTATE:
+            # Check for early stop conditions
             if time.time() - t_start > timeout:
                 self.recognition_server.set_aborted()
-                rospy.loginfo('timeout reached')
-                # Close asr thread
-                asr_audio.put(None)
-                asr_thread.join()
-                return
-                
+                rospy.loginfo('Timeout reached')
+                success = False
+                break
+            elif self.recognition_server.is_preempt_requested():
+                rospy.loginfo('Preempted')
+                self.recognition_server.set_preempted()
+                success = False
+                break
+
             audio_block = audio_in.get()
             sm_vad.input_block(audio_block)
 
@@ -108,69 +113,20 @@ class SpeechRecognitionServer:
                 asr_audio.put(np.frombuffer(audio_block, dtype=np.int16))
         rospy.loginfo('done')
 
-        # End audio caption
+        # End audio capture
         self.mic_manager.disconnect()
         asr_audio.put(None)
 
         # Wait for recognition
         asr_thread.join()
-        recognized_sentence = asr_thread.result
-        #print recognized_sentence
-        #rospy.loginfo("Result: ", recognized_sentence)
-        self.recognition_response.final_result = recognized_sentence
-        self.recognition_server.set_succeeded(self.recognition_response)
-        #self.is_recognizing = False
 
-        # timeout = 15
-        # if goal.timeout:
-        #   timeout = goal.timeout
-        #   rospy.loginfo("I have received a goal with timeout = "+str(timeout))
-        # with sr.Microphone() as source:
-        #   rospy.loginfo('Reconociendo')
-        #   try:
-        #       audio = self.recognizer.listen(source,timeout=timeout)
-        #   except:
-        #       rospy.logerr("Timeout of listening. I recommend to calibrate the recognizer (run uchile_speech_web calibrate.py)")
-        #       self.recognition_server.set_aborted()
-        #       self.is_recognizing = False
-        #       return
-        #   rospy.loginfo('Listoco, I am sending the audio to google. It might take a while')
-        # try:
-        #   recognized_sentence=self.recognizer.recognize_google(audio)
-        #   self.recognition_response.final_result = recognized_sentence
-        #   self.recognition_server.set_succeeded(self.recognition_response)
-        #   print ('Recognized: ' + recognized_sentence)
-        #   self.is_recognizing = False
-        #   return
-        # except sr.UnknownValueError:
-        #   rospy.logwarn("Google Speech Recognition could not understand audio")
-        #   self.recognition_server.set_aborted()
-        #   self.is_recognizing = False
-        # except sr.RequestError as e:
-        #   rospy.logerr("Could not request results from Google Speech Recognition service; {0}".format(e))
-        #   self.recognition_server.set_aborted()
-        #   self.is_recognizing = False
-        # except:
-        #   rospy.logerr("Some error")
-        #   self.recognition_server.set_aborted()
-        #   self.is_recognizing = False
+        if success:
+            recognized_sentence = asr_thread.result
+            #print recognized_sentence
+            #rospy.loginfo("Result: ", recognized_sentence)
+            self.recognition_response.final_result = recognized_sentence
+            self.recognition_server.set_succeeded(self.recognition_response)
 
-
-    # def calibrate(self,goal):
-    #   duration = 1.0
-    #   if goal.duration > 1.0 :
-    #       duration = goal.duration
-    #   if self.is_recognizing:
-    #       self.threshold_server.set_preempted()
-    #       rospy.logwarn("The recognizer is recognizing; I can't stole the power of the mic to calibrate it")
-    #       return
-
-    #   with sr.Microphone() as source:
-    #       rospy.loginfo('Calibrating...')
-    #       self.recognizer.adjust_for_ambient_noise(source,duration=duration)
-    #   self.threshold_server.set_succeeded()
-    #   rospy.loginfo('OK! the mic is calibrated')
-    #   return
 
 if __name__ == '__main__':
     rospy.init_node('recognizer')
